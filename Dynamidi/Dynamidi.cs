9e01dd6ff18fe0c152f662e387c723a49d5c3f7f
jed <jlankitus@handelarchitects.com>
2017-10-21 11:25:44 -0400

    device recognized, but fails to connect

---
+++ Dynamidi/Dynamidi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+using System.Threading;
+using Midi;
+
+///////////////////////////////////////////////////////////////////
+/// NOTE: This project requires references to the DynamoServices
+/// and ProtoGeometry DLLs. These can be found in the latest
+/// ZeroTouch and DynamoServices Nuget packages.
+///////////////////////////////////////////////////////////////////
+
+namespace Dynamidi
+{
+    public class Dynamidi
+    {
+        // Midi variables
+        private const int SysExBufferSize = 128;
+        private static InputDevice midiController = null;
+        private static SynchronizationContext context;
+
+
+        public static string getController()
+        {
+            string output = "";
+
+            if (InputDevice.InstalledDevices.Count == 0)
+            {
+                output = "no device available";
+            }
+            else
+            {
+                try
+                {
+                    InputDevice inputDevice = InputDevice.InstalledDevices[0];
+                    if (!inputDevice.IsOpen)
+                        inputDevice.Open();
+                    inputDevice.StartReceiving(null);
+
+                    output = "device connected";
+                }
+                catch (Exception ex)
+                {
+                    output = "device recognized but failed to connect";
+                    output += "\n";
+                    output += ex.Message.ToString();
+                    output += InputDevice.InstalledDevices.Count.ToString();
+                }
+            }
+
+            return output;
+        }
+    }
+}
